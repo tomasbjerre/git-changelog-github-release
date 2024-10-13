a12aae0ea3e7227118f1812382ac60ee5bcdeb40
Tomas Bjerre <tomas.bjerre85@gmail.com>
2024-10-13 12:30:05 +0200

    breaking: it is alive!

--- README.md
+++ README.md
@@ -2,7 +2,40 @@
 
 This is a
 [GitHub action](https://docs.github.com/en/actions/creating-actions/about-custom-actions)
-that can:
+that can draft release in GitHub generated from template using
+[git-changelog-lib](https://github.com/tomasbjerre/git-changelog-lib).
 
-- Draft release in GitHub generated from template using
-  [git-changelog-lib](https://github.com/tomasbjerre/git-changelog-lib).
+- You can have it running on your **default branch**, not depending on
+  pull-requests.
+- Uses **conventional commits** to keep a **draft release** updated and ready to
+  be published.
+  - It will parse any commit in branch and construct the **draft description**
+    from that.
+  - It will **adjust the draft version** based on conventional commits.
+
+## Usage
+
+- Create a file, perhaps `.github/workflows/draft-release.yaml`, with content:
+
+```yaml
+name: Git Changelog Github Release
+
+on: [workflow_dispatch, workflow_call, push]
+
+jobs:
+  build:
+    runs-on: 'ubuntu-latest'
+    steps:
+      - uses: actions/checkout@v4
+        with:
+          fetch-tags: 'true'
+          fetch-depth: '0'
+      - name: Setup java
+        uses: actions/setup-java@v2
+        with:
+          distribution: 'zulu'
+          java-version: 17
+      - uses: tomasbjerre/git-changelog-github-release@main
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }} # Needs write permission in Github menu '/settings/actions'
+```
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,133 @@
 import * as core from '@actions/core'
-import * as github from '@actions/github'
+import { context, getOctokit } from '@actions/github'
+import { spawnSync } from 'child_process'
+
+function gitChangelogCommandLine(userArgs: string[]) {
+  const { stdout, stderr, status } = spawnSync(
+    'java',
+    ['-jar', __dirname + '/cli.jar', ...userArgs],
+    { encoding: 'utf-8' }
+  )
+  console.log(`stdout: ${stdout}`)
+  console.log(`stderr: ${stderr}`)
+  console.log(`status: ${status}`)
+  if (!status) {
+    return stdout
+  }
+  throw Error(
+    `Error (${status}) from git-changelog-command-line:\n\n${userArgs}\n\n${stderr}\n\n`
+  )
+}
 
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    /**
+     * Auth
+     */
+    const token = process.env.GITHUB_TOKEN ?? process.env.GITHUB_OAUTH2TOKEN
+    if (!token) {
+      throw new Error(`No token specified`)
+    }
+    const { owner, repo } = context.repo
+
+    /**
+     * Config
+     */
+    const draft = core.getInput('draft', { required: false }) === 'true'
+
     const template = core.getInput('template')
     console.log(`Using template:\n\n${template}\n\n`)
+
+    /**
+     * Gather details on release
+     */
+    const highestVersion = gitChangelogCommandLine(['--print-highest-version'])
+    console.log(`highestVersion: ${highestVersion}`)
+
+    const highestVersionTag = gitChangelogCommandLine([
+      '--print-highest-version-tag'
+    ])
+    console.log(`highestVersionTag: ${highestVersionTag}`)
+
+    let currentVersion = gitChangelogCommandLine([
+      '--patch-version-pattern',
+      '^fix.*',
+      '--print-current-version'
+    ])
+    console.log(`Rendered currentVersion:\n\n${currentVersion}\n\n`)
+
+    if (highestVersionTag) {
+      if (currentVersion === highestVersion) {
+        console.log(`No changes made that can be released`)
+        return
+      } else {
+        console.log(
+          `Changes detected and a new ${currentVersion} release can be made`
+        )
+      }
+    } else {
+      console.log(
+        `This is the first version in the repo, using 0.0.1 as version`
+      )
+      currentVersion = '0.0.1'
+    }
+
+    const description = gitChangelogCommandLine([
+      '-std',
+      '--template-content',
+      template
+    ])
+    console.log(`Rendered description:\n\n${description}\n\n`)
+
+    /**
+     * Remove any previous draft
+     */
+    const latestRelease = await getOctokit(token).rest.repos.listReleases({
+      owner,
+      repo
+    })
+    latestRelease.data.forEach(release => {
+      if (!release.draft) {
+        return
+      }
+      console.log(
+        `Removing previous draft (${release.id}):\n\n${release.body_text}\n\n`
+      )
+      getOctokit(token).rest.repos.deleteRelease({
+        owner,
+        repo,
+        release_id: release.id
+      })
+    })
+
+    /**
+     * Create the release
+     */
+    const createReleaseResponse = await getOctokit(
+      token
+    ).rest.repos.createRelease({
+      owner,
+      repo,
+      tag_name: currentVersion,
+      name: currentVersion,
+      body: description,
+      draft,
+      prerelease: false,
+      target_commitish: context.sha
+    })
+
+    const {
+      data: { id: releaseId, html_url: htmlUrl, upload_url: uploadUrl }
+    } = createReleaseResponse
+
+    core.setOutput('id', releaseId)
+    core.setOutput('html_url', htmlUrl)
+    core.setOutput('upload_url', uploadUrl)
+    core.setOutput('version', currentVersion)
+    core.setOutput('description', description)
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    const msg = `Unable to create release. Perhaps the token does not have write-permission, change it in the repo settings '/settings/actions'.`
+    console.log(msg)
+    if (error instanceof Error) core.setFailed(`${msg} ${error.message}`)
   }
 }
